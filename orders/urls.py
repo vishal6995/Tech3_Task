2aebaaff6e8bb6a7e289dc062128b83abb80285d
Vishal S <80169182+vishal6995@users.noreply.github.com>
2021-07-23 00:10:07 +0530

    Update urls.py

--- orders/urls.py
+++ orders/urls.py
@@ -1,13 +1,9 @@
 from django.urls import include, path
-from rest_framework import routers
 from . import views
 
 app_name = "orders"
-router = routers.DefaultRouter()
-router.register(r'api-orders', views.OrderViewSet)
 
 urlpatterns = [
-    path('', include(router.urls)),
     path('orders/', views.ListOrders, name='order_list'),
     path('orders/<int:order_id>/', views.OrderDetails, name='order_details'),
     path('orders/filterstatus', views.filterstatus, name='filterstatus'),
